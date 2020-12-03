fc3975283f6a34e5468bb020a98ba133abd09fce
Chris <praenubilus@gmail.com>
2020-12-03 09:16:17 +0000

    implement prototype

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
+import subprocess
+import json
+import config
+
+
 def main():
-    print("Hello World!")
+    # fetcch data
+    print("\n-------------Start fetching data-------------")
+    r = subprocess.check_output(f"curl {config.DATA_API_URL}", shell=True)
+    str_r = r.decode("utf-8")
+    print("\n-------------Finish fetching data-------------")
+
+    json_data = json.loads(str_r)
+
+    print("Job done")
 
 if __name__ == "__main__":
     main()
